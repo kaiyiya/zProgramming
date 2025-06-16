381a67cca01e2157a0260e38483e6af0775b93f9
zhonglinlin <2194785560@qq.com>
2025-06-16 21:34:38 +0800

    删除倒数第k个节点

---
+++ java/ListNode.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+   class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+class Solution_19 {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(3);
+        l1.next.next.next = new ListNode(4);
+        l1.next.next.next.next = new ListNode(5);
+        l1.next.next.next.next.next = new ListNode(6);
+        l1.next.next.next.next.next.next = new ListNode(7);
+        ListNode l2 = removeNthFromEnd(l1, 2);
+        while (l2 != null) {
+            System.out.println(l2.val);
+            l2 = l2.next;
+        }
+    }
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        
+        
+    }
+}
